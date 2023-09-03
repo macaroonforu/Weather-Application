b2535f3212753179e954a8144be8ac354967a6d8
macaroonforu <macaroonforu@gmail.com>
2023-09-03 17:01:05 -0400

    adding rest of files

--- src/forecast.js
+++ src/forecast.js
@@ -1,6 +1,22 @@
 function forecast(data){
     console.log("Three Day Forecast", data); 
 
+    const three_day_grid = document.getElementById("three-day-grid"); 
+    three_day_grid.innerHTML=''; 
+    for(const day of data){
+        const daydiv = document.createElement('div'); 
+        daydiv.innerHTML = 
+                    `<p>${day.date} </p>
+                    <p>Average Temperature: ${day.day.avgtemp_c} &#176; C</p>
+                    <p>${day.day.condition.text}</p>
+                    <img src=${day.day.condition.icon}>
+                    <p>Daily Maximum: ${day.day.maxtemp_c} &#176; C</p>
+                    <p>Daily Minimum: ${day.day.mintemp_c} &#176; C</p>`
+        three_day_grid.appendChild(daydiv); 
+    }
+
+
+
 }
 
 export default forecast; 
--- src/hourly.js
+++ src/hourly.js
@@ -1,6 +1,19 @@
 function hourly(data){
     console.log("Hourly Data", data); 
 
+    const hourly_grid = document.getElementById("hourly-grid"); 
+    hourly_grid.innerHTML=''; 
+    for(const hour of data){
+        const hourdiv = document.createElement('div'); 
+        hourdiv.innerHTML = 
+                    `<p>${hour.time} </p>
+                    <p>${hour.temp_c} &#176; C</p>
+                    <p>${hour.condition.text}</p>
+                    <img src=${hour.condition.icon}>
+                    `
+        hourly_grid.appendChild(hourdiv); 
+    }
+
 }
 
 export default hourly; 
